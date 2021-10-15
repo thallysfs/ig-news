005b762fbf9122b46580cf206e91eb59ec4f14c7
thallysfs <thallys@hotmail.com>
2021-10-15 16:26:12 -0300

    Criação da página home e api stripper

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,57 @@
+import {GetStaticProps} from 'next'
 import Head from 'next/head'
+import { SubscribeButton } from '../components/SubscribeButton'
+import { stripe } from '../services/stripe'
 
-export default function Home() {
+import styles from './home.module.scss'
+
+interface HomeProps {
+  product: {
+    priceId: string,
+    amount: number
+  }
+}
+
+export default function Home({ product } : HomeProps) {
   return (
     <>
       <Head>
-        <title>Início | ig.News</title>
+        <title>Home | ig.News</title>
       </Head>
-      <h1>
-        Hello world
-      </h1>
+
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>News about the <span>React</span> world.</h1>
+          <p>
+            Get acess to all the publication <br />
+            <span>for {product.amount} month</span>
+          </p>
+          <SubscribeButton priceId={product.priceId} />
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
   )
+}
+
+// forma de usar a chamada a api pelo server side
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve('price_1Jjur3I08lnRLzj61QtvvAlh')
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US',{
+      style: 'currency',
+      currency: 'USD'
+    }).format(price.unit_amount / 100),
+  }
+
+  return {
+    props: {
+      product,
+    },
+    revalidate: 60 * 60 * 24, // 24 horas
+  }
 }
